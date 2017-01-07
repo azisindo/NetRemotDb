26d22482b4ac1fefeceef884241643d6664e75ef
azisindo <aziz.mcgr@gmail.com>
2017-01-07 12:44:10 +0700

    Tambah Idao

---
+++ SmartLibraryLib/IDao/IDao.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartLibraryLib.IDao
+{
+    public interface IDao<T>
+    {
+        int Save(T Entity);
+        int Update(T Entity);
+        int Delete(T Entity);
+
+        List<T> GetAll();
+        DataSet GetReportAll();
+    }
+}
