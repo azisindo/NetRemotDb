da0bad7b3f5d7ae36868edf578d0377971b4a73e
azisindo <aziz.mcgr@gmail.com>
2017-01-07 12:02:20 +0700

    Ok Mulai Start dev

---
+++ SmartLibraryLib/Model/Buku.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartLibraryLib.Model
+{
+    [Serializable]
+    public class Buku
+    {
+        private string _isbn;
+        public string isbn
+        {
+            get { return _isbn; }
+            set { _isbn = value; }
+        }
+
+    }
+}
