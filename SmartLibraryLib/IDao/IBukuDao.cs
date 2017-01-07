6bd6e6681da8e8b85a57f61d304fe4cbf407752d
azisindo <aziz.mcgr@gmail.com>
2017-01-07 13:16:37 +0700

    Ok smartlib

---
+++ SmartLibraryLib/IDao/IBukuDao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SmartLibraryLib.Model;
+
+namespace SmartLibraryLib.IDao
+{
+    public interface IBukuDao:IDao<Buku>
+    {
+        List<Buku> GetByName(string judul);
+        Buku GetById(string bukuId);
+        DataSet GetReportByName(string judul);
+    }
+}
---
+++ SmartLibraryLib/IDao/IPenerbit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using SmartLibraryLib.Model;
+using System.Data;
+
+namespace SmartLibraryLib.IDao
+{
+    public interface IPenerbit:IDao<Penerbit>
+    {
+        List<Penerbit> GetByName(string penerbit);
+        DataSet GetReportByName(string penerbit);
+    }
+}
